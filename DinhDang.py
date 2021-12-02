a79bbcdc08d1e132f0ca7cf01e45147a14edaa38
TTTin239 <79887685+TTTin239@users.noreply.github.com>
2021-12-02 21:30:39 +0700

    Delete DinhDang.py

--- DinhDang.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-img = cv2.imread("paris1.jpg")
-np_img = np.array(img)
-print(f'Kich thuoc ảnh: {np_img.shape}')
-plt.imshow(img[:,:,::-1])
-plt.show()
